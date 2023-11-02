45e6b153e02a6a724dbb33856b8345b4320152ea
E. Ewusi-Essel <45021733+ewusiessel@users.noreply.github.com>
2023-11-02 22:46:21 +0000

    created multiple routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -3,7 +3,25 @@ const router = express.Router();
 
 
 router.route("/").get((req, res) => { 
-    res.status(200).json({message: "Get all f**king contacts"}); 
+    res.status(200).json({message: "Get all contacts"}); 
 });
+
+router.route("/:id").get((req, res) => { 
+    res.status(200).json({message: `Get a contact for ${req.params.id}`}); 
+});
+
+
+router.route("/").post((req, res) => { 
+    res.status(200).json({message: "Create Contact"}); 
+});
+
+router.route("/:id").put((req, res) => { 
+    res.status(200).json({message: `Update contacts for ${req.params.id}`}); 
+});
+
+router.route("/:id").delete((req, res) => { 
+    res.status(200).json({message: `Delete contact for ${req.params.id}`}); 
+});
+
 
 module.exports = router;
